3c81f5e68b204e65edc4541ddd6bf5ac9f029674
Kirill Bazhenov <bazhenovc@bazhenovc-laptop>
2013-03-09 02:37:23 +0200

    proper name for CB projects

--- src/codeblocksgenerator.cc
+++ src/codeblocksgenerator.cc
@@ -6,13 +6,15 @@
 // At the moment - only linux custom makefile project
 void CodeBlocksGenerator::generate(Parser& parser)
 {
-	std::ofstream ofs("cb10-makefile.cbp");
+    std::string projectName = parser.fileName.substr(0,
+                                                     parser.fileName.find_last_of("."));
+	std::ofstream ofs(projectName + "-makefile.cbp");
 	if (ofs.is_open()) {
 		ofs << "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\" ?>" << std::endl
 			<< "<CodeBlocks_project_file>" << std::endl
 			<< "<FileVersion major=\"1\" minor=\"6\" />" << std::endl
 			<< "<Project>" << std::endl
-			<< "<Option title=\"Project\" />" << std::endl
+			<< "<Option title=\""<< projectName << "\" />" << std::endl
 			<< "<Option makefile_is_custom=\"1\" />" << std::endl
 			<< "<Build>" << std::endl
 			<< "<Target title=\"all\"></Target>" << std::endl
--- src/parser.hh
+++ src/parser.hh
@@ -8,35 +8,38 @@ struct Target;
 class Parser
 {
 public:
-    
+
     Parser()
     : mCurrentTarget(0)
     {}
-    
+
     ~Parser();
-    
+
     inline void parse(const char* filename)
     {
         std::ifstream inf(filename);
-        
+        fileName = filename;
+
         if (inf.is_open())
             parseRecursive(inf, "");
     }
-    
+
     void dump() const;
-    
+
     inline const std::list<std::string>& getSectionContents(const std::string& name)
     {
         return mContents[name];
     }
-    
+
     inline const std::list<Target*>& getTargets() const
     {
         return mTargets;
     }
-    
+
+    std::string fileName;
+
 private:
-    
+
     std::list<Target*> mTargets;
     Target* mCurrentTarget;
     std::map<std::string, std::list<std::string> > mContents;
